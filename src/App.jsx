60bb4647d9a630afd7aad382b3d525462a359e48
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-24 18:24:41 +0100

    finish writing data with React router action

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Menu, {loader as menuLoader} from './features/menu/Menu'
 
 import Cart from './features/cart/Cart'
 
-import CreateOrder from './features/order/CreateOrder'
+import CreateOrder , {action as createOrderAction} from './features/order/CreateOrder'
 import Order, {loader as orderLoader} from './features/order/Order'
 import AppLayout from "./ui/AppLayout"
 import Error from './ui/Error'
@@ -49,7 +49,8 @@ const router= createBrowserRouter([
       },
       {
         path: '/order/new',
-        element: <CreateOrder />
+        element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
 import { useState } from "react";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 // eslint-disable-next-line no-unused-vars
@@ -34,15 +36,37 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState('off');
   // eslint-disable-next-line no-unused-vars
   const cart = fakeCart;
+
+  function handlePriority(state) {
+    console.log(state)
+    const checkState= state ? 'on': 'off';
+    setWithPriority(checkState)
+  }
 
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      {/* from react router dom */}
+      {/* <Form method="POST" action="/order/new"> */}
+
+      {/* 
+        - So notice how this entire form right here works complitely without any
+        javaScript and and without OnSubmit handler for example. 
+        - So all we have is this Form here and then react router takes care of 
+        the rest we also didn't have to create any state variables here for each of 
+        these input fields and didn't even create a loading state. so react router 
+        will do all of these automatically without us having to do anything. 
+        - The idea behind all these is to basically allow us to go back to the 
+        basics so to the way HTML used to work back in the day before everyone 
+        starting using JavaScript for the frontend. 
+        - So back then we simply created HTML FORM similar to this one and then 
+        when we submited a request was send to the server which then did to work. 
+       */}
+      <Form method="POST">  
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -67,18 +91,47 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => handlePriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
           <button>Order now</button>
-        </div>
-      </form>
+        </div> 
+      </Form>
     </div>
   );
+}
+
+export async function action({request}){
+  const formData= await request.formData();
+  const data= Object.fromEntries(formData);
+  // console.log(data);
+
+  const order= {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  }
+
+  const newOrder =  await createOrder(order);
+
+
+  /*
+    - we want to do is to immediatly redirect the page to the order/:id
+    basically showing the user the information about that new order. 
+    - we can't do it using the navigate function because it comes from calling
+    the useNavigate() hook but we can not call hooks inside this function so hooks 
+    can only to be called inside components.
+    - And so here we need to use another function which is called "redirect()" 
+    - so this is basically another function that's provided to us by react router 
+    which basically will just create a new response or a new request.  
+  */ 
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
