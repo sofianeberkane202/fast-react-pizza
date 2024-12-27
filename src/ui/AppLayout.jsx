7b28db7d42d4f12a8471f93342827773768e2a44
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-27 19:33:01 +0100

    finish styling each files component in ui folder

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,27 +1,23 @@
-import Header from "./Header"
-import CartOverview from '../features/cart/CartOverview'
-import { Outlet, useNavigation } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 function AppLayout() {
+  const navigation = useNavigation();
 
-    const navigation= useNavigation();
+  const isLoading = navigation.state === "loading";
 
-    const isLoading = navigation.state === 'loading';
-
-    return (
-        <div className="layout">
-            
-            <Header/>
-
-            <main>
-                { isLoading && <Loader/> }
-                {!isLoading && <Outlet/>}
-            </main>
-
-            <CartOverview/>
-
-        </div>
-    )
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      <Header />
+      {isLoading && <Loader />}
+      {/* {true && <Loader />} */}
+      <div className="overflow-scroll">
+        <main>{!isLoading && <Outlet />}</main>
+      </div>
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../features/order/SearchOrder"
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
-    return (
-        <header>
-            <Link to='/'>Fast React Pizza Co.</Link>
-            <SearchOrder/>
-            <p>sofiane</p>
-        </header>
-    )
+  return (
+    <header className="flex flex-col items-center space-y-2 border-b-4 border-stone-300 bg-yellow-500 px-4 py-3 font-sans uppercase sm:flex-row sm:justify-between sm:px-6">
+      <Link to="/" className="w-fit tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
